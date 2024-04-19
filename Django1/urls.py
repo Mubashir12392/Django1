36567adc36048f7a747f032b8b15bc631c73be10
Mubashir12392 <mubashirhaider01@gmail.com>
2024-04-20 02:36:11 +0500

    remove extra imports from urls.py

--- Django1/urls.py
+++ Django1/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from djangoforms.views import MyView,success
+
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
