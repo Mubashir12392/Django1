5cc4b5d2ae543186ed91fbe4e9884b7d8b7dc898
Mubashir12392 <mubashirhaider01@gmail.com>
2024-04-21 15:03:30 +0500

    Add Some urls in it

--- djangoforms/urls.py
+++ djangoforms/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import MyView, Mysuccess
+from .views import StudentView, student_list, student_detail, StudentUpdate
 
 urlpatterns = [
-    path('myform/', MyView.as_view()),
-    path('myform/mysuccess/', Mysuccess)
+    path('studentform/', StudentView.as_view()),
+    path('students/', student_list, name='students'),
+    path('student/<int:id>/',student_detail, name='student_detail'),
+    path('student/<int:pk>/update', StudentUpdate.as_view(), name='student_update')
+    
 ]
