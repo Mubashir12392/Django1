765c843be5b67aff5b365e588bb627fcde5ed354
Mubashir12392 <mubashirhaider01@gmail.com>
2024-04-21 15:02:42 +0500

    1. Change MyView Name. 2. Remove Mysuccess view 3. Add Student_list View 4. Add Student_detail view 5. Add StudentUpdat View

--- djangoforms/views.py
+++ djangoforms/views.py
@@ -1,15 +1,18 @@
+from django.db.models.base import Model as Model
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.views.generic import FormView
-from .forms import MyForm
-from .models import MyModel
+from django.urls import reverse_lazy
+from django.views.generic import FormView, UpdateView, DeleteView
+from .forms import StudentForm
+from .models import Student
 
 # Create your views here.
 
-
-class MyView(FormView):
-    template_name = 'myform.html'
-    form_class = MyForm
-    success_url = 'mysuccess/'
+# CRUD ----- CREATE
+class StudentView(FormView):
+    template_name = 'studentform.html'
+    form_class = StudentForm
+    success_url = reverse_lazy ('students')
 
     def form_valid(self, form):
 
@@ -20,10 +23,36 @@ class MyView(FormView):
         message = form.cleaned_data['message']
 
         # Create and save the new instance of the model
-        MyModel.objects.create(name=name, email=email, gender=gender, message=message)
+        Student.objects.create(name=name, email=email, gender=gender, message=message)
 
         return super().form_valid(form)
     
 
-def Mysuccess(request):
-    return render(request, 'mysuccess.html')
+# CRUD ------ READ
+def student_list(request):
+    students = Student.objects.all()
+    context = {
+        'students' : students
+    }
+    return render(request, 'studentlist.html', context=context)
+
+def student_detail(request, id):
+    student = Student.objects.get(id=id)
+    context = {
+        'student' : student
+    }
+    return render(request, 'studentdetail.html', context=context)
+
+
+# CRUD ----- UPDATE
+
+class StudentUpdate(UpdateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'studentform.html'
+    success_url = reverse_lazy('students')
+
+    def get_object(self, queryset=None):
+        # Get the student object to update
+        return self.model.objects.get(id=self.kwargs['pk'])
+
