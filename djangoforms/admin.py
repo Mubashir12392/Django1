609bade746724869937390cc5044f69b3d404394
Mubashir12392 <mubashirhaider01@gmail.com>
2024-04-21 14:59:31 +0500

    change the name of model in djangoforms app file admin.py

--- djangoforms/admin.py
+++ djangoforms/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import MyModel
+from .models import Student
 
 # Register your models here.
 
-@admin.register(MyModel)
+@admin.register(Student)
 class MyModelData(admin.ModelAdmin):
     pass
