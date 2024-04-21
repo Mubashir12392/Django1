90a3da6886ba312cae4019788ec6f7a191206d67
Mubashir12392 <mubashirhaider01@gmail.com>
2024-04-21 14:58:18 +0500

    change model name to Student in djangoforms app

--- djangoforms/models.py
+++ djangoforms/models.py
@@ -2,22 +2,18 @@ from django.db import models
 
 # Create your models here.
 
-class MyModel(models.Model):
+class Student(models.Model):
 
-    CHOICES = (
-        ('M','Male'),
-        ('F','Female'),
-        ('O','Others')
-        )
+
 
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, unique=True)
-    gender = models.CharField(max_length=50, choices=CHOICES)
+    gender = models.CharField(max_length=50)
     message = models.TextField(null=True, blank=True)
 
     class Meta:
-        verbose_name = ("MyModel")
-        verbose_name_plural = ("MyModels")
+        verbose_name = ("Student")
+        verbose_name_plural = ("Students")
 
     def __str__(self):
         return self.name
