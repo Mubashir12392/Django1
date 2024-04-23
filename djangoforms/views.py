b2fb99b508d028d8c55e4a1dff497931054bb28b
Mubashir12392 <mubashirhaider01@gmail.com>
2024-04-24 00:26:17 +0500

    1. Add login Required Mixin in CBGV

--- djangoforms/views.py
+++ djangoforms/views.py
@@ -1,6 +1,8 @@
 from django.db.models.base import Model as Model
 from django.urls import reverse_lazy
 from django.views.generic import CreateView,ListView,DetailView,UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .forms import StudentForm
 from .models import Student
 
@@ -8,27 +10,27 @@ from .models import Student
 # Create your views here.
 
 # CRUD ----- CREATE
-class StudentAdd(CreateView):
+class StudentAdd(LoginRequiredMixin, CreateView):
     model = Student
     form_class = StudentForm
     template_name = 'studentform.html'
     success_url = reverse_lazy('students')
 
 # CRUD ------ READ
-class Studentlist(ListView):
+class Studentlist(LoginRequiredMixin, ListView):
     model = Student
     template_name = 'studentlist.html'
     context_object_name = 'students'
 
 
-class StudentDetail(DetailView):
+class StudentDetail(LoginRequiredMixin, DetailView):
     model = Student
     template_name = 'studentdetail.html'
     context_object_name = 'student'
 
 
 # CRUD ----- UPDATE
-class StudentUpdate(UpdateView):
+class StudentUpdate(LoginRequiredMixin, UpdateView):
     model = Student
     form_class = StudentForm
     template_name = 'editstudent.html'
@@ -37,7 +39,7 @@ class StudentUpdate(UpdateView):
 
 
 # CRUD ----- DELETE
-class StudentDelete(DeleteView):         # Delete button in template must send post request to delete 
+class StudentDelete(LoginRequiredMixin, DeleteView):         # Delete button in template must send post request to delete 
     model = Student
     template_name = 'studentdetail.html'
     success_url = reverse_lazy('students')
