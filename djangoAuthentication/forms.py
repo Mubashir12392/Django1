8b53e632016d8b875e07fa59b3907fdbb87a2441
Mubashir12392 <mubashirhaider01@gmail.com>
2024-04-24 00:18:02 +0500

    1. Some Changes in CustomUserForm 2. Add Custom Signin Form

--- djangoAuthentication/forms.py
+++ djangoAuthentication/forms.py
@@ -1,8 +1,18 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
 
-class CustomUserForm(UserCreationForm):
-    first_name = forms.CharField(label='First Name', max_length=20)
-    last_name = forms.CharField(label='Last Name', max_length=30)
-    email = forms.EmailField(label= 'Email', required=True)
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+
+# --- Custom User Sign Up Form
+
+class CustomSignUpForm(UserCreationForm):
+    # We can add here our custom Changes
+    class Meta:
+        model = User
+        fields = ['username','first_name','last_name','email']
     
+    
+# ---- Custom User Sign in Form
+
+class CustomSignInForm(AuthenticationForm):
+    pass             # We can also Customize it
