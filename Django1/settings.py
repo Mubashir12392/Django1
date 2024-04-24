bebf4a56b8f6c21d8ce245a329c914391eb54db9
Mubashir12392 <mubashirhaider01@gmail.com>
2024-04-24 16:45:17 +0500

    Change database Sqlite to Postgres

--- Django1/settings.py
+++ Django1/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'Django1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mubashir12',
+        'PASSWORD': '334334',
+        'HOST':'localhost',
+        'PORT': '5432'
     }
 }
 
