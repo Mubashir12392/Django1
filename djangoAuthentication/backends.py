bb392935172a41b42e58129fb113bc41cea2a66a
Mubashir12392 <mubashirhaider01@gmail.com>
2024-04-25 23:59:50 +0500

    Add Custom Authentication

---
+++ djangoAuthentication/backends.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+
+class CustomAuthBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        User = get_user_model()
+        try:
+            user = User.objects.get(username=username)
+            if user.username.startswith('M') and user.check_password(password):
+                return user
+            else:
+                return None
+        except User.DoesNotExist:
+            return None
