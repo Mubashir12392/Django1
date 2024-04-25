b92ccd8f1a736c16dd39867b468d98f34a5c9f33
Mubashir12392 <mubashirhaider01@gmail.com>
2024-04-26 01:08:08 +0500

    Made some changes in custom authentication

--- djangoAuthentication/backends.py
+++ djangoAuthentication/backends.py
@@ -1,14 +1,17 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 
+
 class CustomAuthBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         User = get_user_model()
         try:
             user = User.objects.get(username=username)
-            if user.username.startswith('m') and user.check_password(password):
+            if user.username[0].lower() in ['m', 'a'] and user.check_password(password):
                 return user
             else:
+                
                 return None
         except User.DoesNotExist:
+            print("User does not exist")
             return None
